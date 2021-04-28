95608e75e7666042e4566169a5f418bad52cb0ef
Orestis Ousoultzoglou <xlxs4@protonmail.ch>
2021-04-29 00:00:45 +0300

    Feature extraction, default RMA and gcRMA normalization

---
+++ differential-expression-analysis/dea.R
@@ -0,0 +1,42 @@
+library(affy)
+library(arrayQualityMetrics)
+library(here)
+library(gcrma)
+
+
+raw_data_dir <- here(
+    "data",
+    "raw",
+    "GLDS-62_microarray_E-GEOD-64468.raw.1"
+)
+qc_data_dir <- here("qc")
+
+cel_affybatch <- ReadAffy(filenames = list.celfiles(
+    raw_data_dir,
+    full.names = TRUE
+))
+
+arrayQualityMetrics(
+    expressionset = cel_affybatch,
+    outdir = here(qc_data_dir, "QC_Report_Affybatch"),
+    force = TRUE,
+    do.logtransform = TRUE
+)
+
+
+eset_rma <- rma(cel_affybatch)
+arrayQualityMetrics(
+    expressionset = eset_rma,
+    outdir = here(
+        qc_data_dir,
+        "QC_Report_RMA"
+    ),
+    force = TRUE
+)
+
+eset_gcrma <- gcrma(cel_affybatch, type = "fullmodel")
+arrayQualityMetrics(
+    expressionset = eset_gcrma,
+    outdir = here(qc_data_dir, "QC_Report_gcRMA"),
+    force = TRUE
+)
