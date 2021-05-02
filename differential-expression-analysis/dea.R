cd413b0f452e7e6671aa01cd03fe8a62e59b23cc
Orestis Ousoultzoglou <xlxs4@protonmail.ch>
2021-05-03 00:08:37 +0300

    Annotate eset; filter multiple mappings probes
    using ENSEMBL, which in yeast2.db
    "contains mappings between Entrez Gene identifiers and Ensembl gene
    accession numbers"

--- differential-expression-analysis/dea.R
+++ differential-expression-analysis/dea.R
@@ -1,3 +1,5 @@
+# Import order matters for masking, take care.
+library(dplyr)
 library(affy)
 library(AnnotationDbi)
 library(arrayQualityMetrics)
@@ -7,7 +9,7 @@ library(yeast2probe)
 
 
 # Make sure to launch the R process from the
-# project’s top-level directory for here() to work.
+# project top-level directory for here() to work.
 # If you can't do that, feel free to play with rprojroot:
 # https://github.com/jennybc/here_here#tldr
 
@@ -75,15 +77,34 @@ arrayQualityMetrics(
     force = TRUE
 )
 
-# List all data retreivable from the ChipDb AnnotationDb.
-columns(yeast2.db)
-# Example annotation.
+# Adapted from maEndtoEnd
+# http://bioconductor.org/packages/devel/workflows/html/maEndToEnd.html
+# An end to end workflow for differential gene expression
+# using Affymetrix microarrays
 annotated_data <- select(
     yeast2.db,
-    # keys = (featureNames(eset_rma)),
-    keys = head(keys(yeast2.db, keytype = "PROBEID")),
-    columns = c("ALIAS", "GENENAME"),
+    keys = featureNames(eset_rma),
+    columns = c("PROBEID", "ENSEMBL", "GENENAME"),
     keytype = "PROBEID"
 )
-annotated_data <- subset(annotated_data, !is.na(ALIAS))
-head(annotated_data)
+# Filter out probes that do not map to a gene.
+# yeast2.db doesn't have SYMBOL.
+# However, ENSEMBL also maps to ENTREZ.
+annotated_data <- subset(annotated_data, !is.na(ENSEMBL))
+
+# Grab transcript-cluster identifiers that map to multiple gene identifiers.
+annotated_grouped <- group_by(annotated_data, PROBEID)
+annotated_summarized <-
+    dplyr::summarize(annotated_grouped, no_of_matches = n_distinct(ENSEMBL))
+annotated_filtered <- filter(annotated_summarized, no_of_matches > 1)
+probe_stats <- annotated_filtered
+
+# Generate an expression set without the probes with multiple mappings.
+ids_to_exclude <- (featureNames(eset_rma) %in% probe_stats$PROBEID)
+eset_final <- subset(eset_rma, !ids_to_exclude)
+
+# Get the feature data only for the filtered probes.
+fData(eset_final)$PROBEID <- rownames(fData(eset_final))
+fData(eset_final) <- left_join(fData(eset_final), annotated_data)
+# Restore the rownames after performing the left join.
+rownames(fData(eset_final)) <- fData(eset_final)$PROBEID
