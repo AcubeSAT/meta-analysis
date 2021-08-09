e73b1d471185ed10ce2b2877831ba1b8c2417c2f
Orestis Ousoultzoglou <xlxs4@protonmail.ch>
2021-08-09 13:00:36 +0000

    Add logging in GSE4136 script

--- differential-expression-analysis/src/irradiation.R
+++ differential-expression-analysis/src/irradiation.R
@@ -1,14 +1,16 @@
+library(logger)
+
+log_info("Importing libraries...")
+
 library(GEOquery)
 library(limma)
 library(umap)
 # implicit dependencies
 library(statmod)
 
-# load series and platform data from GEO
-
+log_info("Loading data from GEO...")
 gset <- getGEO("GSE4136", GSEMatrix = TRUE, AnnotGPL = TRUE)
-if (length(gset) > 1) idx <- grep("GPL2529", attr(gset, "names")) else idx <- 1
-gset <- gset[[idx]]
+gset <- gset[[1]]
 
 # make proper column names to match toptable
 fvarLabels(gset) <- make.names(fvarLabels(gset))
@@ -22,6 +24,7 @@ sel <- which(sml != "X")
 sml <- sml[sel]
 gset <- gset[, sel]
 
+log_info("log2 transforming...")
 # log2 transformation
 ex <- exprs(gset)
 qx <- as.numeric(quantile(ex, c(0., 0.25, 0.5, 0.75, 0.99, 1.0), na.rm = T))
@@ -38,19 +41,26 @@ groups <- make.names(c("test", "control"))
 levels(gs) <- groups
 gset$group <- gs
 
-design <- model.matrix(~ group + 0, gset)
-colnames(design) <- levels(gs)
+log_info("Creating design matrix...")
+design_matrix <- model.matrix(~ group + 0, gset)
+colnames(design_matrix) <- levels(gs)
 
+log_info("Fitting linear models...")
 # fit linear model
-fit <- lmFit(gset, design)
+fit <- lmFit(gset, design_matrix)
 
 # set up contrasts of interest and recalculate model coefficients
 cts <- paste(groups[1], groups[2], sep = "-")
-cont_matrix <- makeContrasts(contrasts = cts, levels = design)
+log_info("Creating contrast matrix...")
+cont_matrix <- makeContrasts(contrasts = cts, levels = design_matrix)
+
+log_info("Re-orientating fitteed model to the set of contrasts...")
 fit2 <- contrasts.fit(fit, cont_matrix)
 
-# compute statistics and table of top significant genes
+log_info("Computing statistics and metrics by empirical Bayes...")
 fit_eb <- eBayes(fit2, robust = TRUE)
+
+log_info("Selecting DE genes...")
 tt <- topTable(fit_eb,
     adjust.method = "BH",
     sort.by = "B",
@@ -59,13 +69,15 @@ tt <- topTable(fit_eb,
     lfc = 2
 )
 
-# summarize test results as "up", "down" or "not expressed"
+log_info("Running decideTests...")
+# Identify the significantly differentially expressed genes for each contrast.
 results <- decideTests(fit_eb,
     adjust.method = "BH",
     p.value = .05,
     lfc = 2
 )
 
+log_success("The script finished running successfully!")
 # ct <- 1
 # volcanoplot(fit_eb,
 #     coef = ct,
